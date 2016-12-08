f7cc3f5fc56a595049aaed064caa1b3c24d8842d
AndreaFloro <andreafloro@outlook.it>
2016-12-08 16:38:44 +0100

    Engine Test app project setup

---
+++ TestApp/Src/Main.cpp
@@ -0,0 +1,13 @@
+#pragma comment(lib, "Engine")
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN       
+#endif // !WIN32_LEAN_AND_MEAN
+
+#include <Windows.h>
+
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	return 0;
+}
